fbd639b898dce1933dc18ab1658ad218b3b55174
Kamil Jarmołowicz <100926787+XennaExtra@users.noreply.github.com>
2022-03-11 10:32:54 +0000

    iugfiujiufc

--- main.py
+++ main.py
@@ -1,70 +1,238 @@
-### Zapoznaj się z prostymi typami zmiennych
-# print('Hello kognitywistyka') # funkcja (dalej jako fun) print wyświetla wynik na ekranie
+from os import system
+
+clear = lambda: system('clear')
+
+
+## Język python jest językiem wysokiego poziomu, typowanie danych w Pythonie jest dynamiczne (interpreter sam rozpoznaje typ zmiennej na podstawie jej wartości)
+## You should always use camelCase for class names, snake_case for everything else in Python (you could to use UPPER_CASE_SNAKE for constants).
+############ Zmienne; typ prosty (klasy): string, integer, float, bool ###############
+# print('Hello world')  
+
+# var_string = 'mercedes'    
+# print(type(var_string)) # funkcja type sprawdzenie typu zmiennej
+
+# var_bool = True
+# print(type(var_bool))   
+
+# var_integer = 1
+# print(type(var_integer)) 
+
+# var_float = 2.2324234
+# print(type(var_float)) 
+
+#### przypisanie wielu wartości do wielu zmiennych
+# a, b, c = 1, 2, 1+2
+# print(a,b,c)
+# print('a = ', a, '; b = ', b, '; c =', c)
+# print('a = ' + str(a) + ', b = ' + str(b) + ', c =' + str (c))   # str - konwersja zmiennej na typ string 
+# print('a = {}; b = {}; c = {}'.format(a,b,c))
+
+########################### Importowanie pakietów/bibliotek w Python
+# Sposób 1
+# import math   # importuje wszystkie funkcje i metody dostępne w bibliotece math
+# x = 30*math.pi/180
+# print(math.sin(x))
+
+# Sposób 2
+# from math import pi, sin 
+# x = 30*pi/180
+# print('30*pi/180 =', sin(x))
+
+############ strukturalny vs funkcyjny
+## Oblicz pierwiastek z liczby 4
+# p1 = 4**(1/2)  # paradygmat programowania strukturalnego
+# print(p1)
+
+# from math import sqrt
+# p2 = sqrt(4) # paradygmat programowania funkcyjnego
+# print(p2)  
+
+## Komunikacja z użytkownikiem, wczytanie danych z konsoli
+# name = input('Podaj imię: ')  
+# print(name)
+# age = int(input('Podaj wiek: '))   # int - konwersja typu zmiennej string (str, łańcuch) na integer (int, liczba całkowita)
+# print(name + ' ' + str(age))    # str - konwersja typu zmiennej str na int
+
+# print("Twoje imię to: {} masz {} lat.".format(name, age))
+
+###### Zmienne; typy złożone
+# W Pythonie nie ma tablic
+# # ################## Lista  ####################
+# Tworzenie listy i odwoływanie się do jej elementów
+# list_empty1 = list()  # pusta lista   
+# list_empty2 = []      # też pusta lista   
+# list1 = ['Informatyka', 1, True]  # to jest lista o nazwie cars
+# print(list1)
+# print(list1[0]) # uwaga numeracja wektora/listy itd. jest od 0 
+# print(list1[2]) # tu element nr 3 listy
+########################### metody dla klasy lista ##########################
+####### metody (okreslone operacje) wykonujemy na obiektach, obiektem może być np. nasza zmienna
+# # # Zapoznaj się z kilkoma poniższymi podstawowymi i użytecznymi metodami dla list
+# # # append(x) - Dodaje element do końca listy,
+# # # extend(L) rozszerza listę poprzez dołączenie wszystkich elementów podanej listy L,
+# # # insert(i, x) - umieszcza element x na podaną pozycję listy i, np. nazwaLista.insert(0,'Ala')
+# # # remove(x) - usuwa pierwszy napotkany element z listy, którego wartością jest x.
+# # # pop([i]) - usuwa element z podanej pozycji na liście i zwraca go jako wynik.
+# # # index(x) - zwraca indeks pierwszego elementu listy, którego wartością jest x, np. cars.index('BMW')
+# # # count(x) - zwraca liczbę wystąpień elementu x na liście.
+# # # sort() - sortuje elementy na liście, w niej samej
+# # # reverse() - odwraca porządek elementów listy.
+##########################################################################
+# # # Zapoznaj się przykładami zastosowania w/w metod ###############################################
+# cars = ['Audi', 'BMW', 'Mercedes','BMW', 'BMW']
+# print(cars)
+# cars.append('Porsche') # na obiekcie o nazwie cars wywołana została metoda o nazwie append
+# print(cars)
+
+#cars = ['Audi', 'BMW', 'Mercedes','BMW']
+# print(cars)
+# cars.insert(2,'Porsche')
+# cars2 = cars
+# print(cars2)
+
+#######  Użycie metody index na obiekcie cars, sprawdzamy index elementu listy
+# cars = ['Audi', 'BMW', 'Mercedes','BMW']
+# print(cars)
+# ind = cars.index('BMW') # na obiekcie cars stosujemy metodę index, 1-sza pozycja elementu
+# print(ind)
+# ind2 = cars.count('BMW') # na obiekcie cars stosujemy metodę count liczba wystąpień elementu
+# print(ind2)
+
+# # Usuwanie wybranego elementu z listy
+# numbers = [1, 2, 3, 10, 18, 10, 13, 10]
+# print(numbers)
+# numbers.remove(10)
+# print(numbers)
+
+### Łączenie list
+# list1 = ["Ala", "Zosia"]
+# list2 = ["Janek", "Tomek"]
+# merge_list = list1 + list2
+# print(merge_list)
 #
-## nazwa zmiennej powinna mieć znaczenie, staraj się nie używać pojedyńczych liter a,b,c, lub liter+numer  x1,x2,x3
-## kod w j.polskim lub angielskim, mix w/w jest możliwy ale nieprofesjonalny
-## Konwencja w zapisie nazw zmiennych: snake vs camel  wybierz 1 z nich
-'''
-Raw: user login count
-Camel Case: userLoginCount
-Snake Case: user_login_count
-'''
-# name_of_car = 'mercedes'  # zmienna name_of_car jest typu string
-# print(name_of_car) # 
-# print('Lubię samochód marki mercedes a moja rodzina też lubi samochód marki: ' + name_of_car)  
+# #Zadanie 1
+# Utwórz listę z imionami (conajmniej 10 imion, część powinna się powtarzać)
+# określ indeks (numer wiersza) w której znajduje się imie osoby, nazwę osoby podaje użytkownik
+# ile osób o imieniu wskazanym przez użytkownika znajduje się na liście
+# dołącz nowe imie do listy do końca listy
+# dołącz nowe imię jako 3 pozycję na liście
+# posortuj obiekty w liście, usuń ostatni element z listy
+# utwórz nową listę z 3 imionami i dołącz do listy
 
-############Zmienne numeryczne vs łańcuchowe (łańcuch znaków = wyraz, tekst) ###############
-# num1 = 2  # zmienna a ma typ integer (liczba całkowita)
-# num2 = 7.345134   # zmienna b typ float (liczba zmiennoprzecinkowa)
-# num3 = num1 + num2
-# # pot_num2_num3 = num2 ** num3  # ** to operator potęgowania  num2^num3
-# # print(num1)
-# # print(num2)
-# print(num3)
-# # print(num4)
-# print(type(num2)) # fun type sprawdza typ zmiennej
+firstNames = ["Piotr", "Basia", "Ela", "Kamil", "Tomasz", "Piotr", "Janusz", "Janusz", "Paulina", "Stanisław", "Piotr"]
+userFirstName = input("Podaj swoje imię: ")
 
-# print("#####################")
-z = 'Ala '
-x = "ma "
-y = "kota"
-zdanie = z + x + y
-# print(zdanie)
-#print(y,x,z)
-# X = 2     # python rozróżnia małe i duże litery (X to nie to samo co x)
-# Y = '2'
-# print("Zmienna X ma typ: ",type(X))
-# print("Zmienna Y ma typ: ",type(Y))
-# print(z,"ma kotów: ",str(X))
-# print(z,"ma kotów: ",Y)
-# print(X+Y)
+userFirstNameIndex = firstNames.index(userFirstName)
+userFirstNameCount = firstNames.count(userFirstName)
+print(f"Name index: {userFirstNameIndex}, name exists: {userFirstNameCount} times.")
+firstNames.append(userFirstName)
+firstNames.insert(2, userFirstName)
+print(firstNames)
+firstNames.sort()
+print(firstNames)
+firstNames.pop(len(firstNames) - 1)
+print(firstNames)
+newNameList = ["Katarzyna", "Magdalena", "Barbara"]
+firstNames.extend(newNameList)
+print(firstNames)
 
-### Komunikacja/interakcja z użytkownikiem
-### Wpisz w konsoli liczby, zwróć uwagę na różnice między zmienną numeryczną i łańcuchową 
-# zm1 = input("Podaj zmienną x =") # fun input służy do wprowadzania wartości przez uzytkownika w konsoli
-# zm2 = input("Podaj zmienną y =") # wynik funkcji (argument wyjściowy funkcji) input ma typ string
-# zm3_string = zm1 + zm2  # konwersję zmiennej string na  float
-# print(zm3_string)
 
-# zm1 = input("Podaj zmienną x =") # fun imput służy do wprowadzania wartości przez uzytkownika w konsoli
-# zm2 = input("Podaj zmienną y =") # wynik funkcji (argument wyjściowy funkcji) input ma typ string
-# zm3 = float(zm1) + float(zm2)  # konwersję zmiennej string na  float
-# print(c)
 
-############ Zmienne logiczne ###############
-# a = True
-# b = False # zmienna b typ logiczny bool
-# print(type(a))
-# x = '1'
-# y = 1
-# print(x == y)   # operator ==  oznacza czy x równe y?
-# print(x != y)   # operator !=  oznacza czy x różny od y?
-# a = 3
-# b = 5
-# print(a < b)
-# print(a >= b)
+# # ################# Klasa: Krotka (Tuple)
+# print('To moja pierwsza krotka')
+# cars2 = ('A', 'B')  # krotka = tuple
+# print(cars2)
+# #cars2.append('aaaaa')   # w krotce nie mogę zmieniać wartości, elementów
+# print(cars2, type(cars2))
 
-##############Zadania do wykonania, Twoje pierwsze algorytmy
+# ################  Klasa: Słownik
+# dict_empty1 = dict()    # pusty słownik
+# print(dict_empty1)
+# print(type(dict_empty1)) 
+
+#słownik = {klucz1: wartość klucza1, klucz2: wartość klucza2, itd}
+# slownik1 = {'wojenne': 'Medal od honor, Call of Duty',
+#            'romans ': 'Romeo i Julia',
+#            'komiksy ': 'Kaczor Donald'}
+# print(slownik1)
+
+# cars_countries = {
+#       'Germany': ['Audi', 'BMW'],
+#       'Asia': {'Japan': ['Toyota', 'Honda']}
+# }
+# print(cars_countries)
+#### Przykład: Zwróć uwagę na odwoływanie się do elementów słownika, po nazwie klucza, a
+##### następnie po elementach listy (wartość klucza)
+# print(cars_countries['Germany'])
+# print(cars_countries['Germany'][1])
+# print(cars_countries['Asia']['Japan'][0])
+# print(cars_countries['Korea'])  # nas slownik nie ma takiego klucza
+
+######################Zadanie 2
+# Utwórz słownik zawierający  trzy klucze: imie, nazwisko, wiek
+# jako wartości w/w kluczy wpisz listy 3-elementowe zawierające dowolne dane osobowe
+# następnie wyświetl kompletne dane osoby o numerze wskazanej przez użytkownika
+
+personDictionary = {
+  'imie': ["Jan", "Mateusz", "Paweł"], 
+  'nazwisko': ["Kowalski", "Nowak", "Kowalski"],
+  'wiek': [50, 30, 15]
+}
+personDictionary.update({
+ 'kierunek_studiów': ["Informatyka", "Chemia", "Farmacja"] 
+})
+userInput = -1
+personList = list(personDictionary.values())
+listLength = len(personList[0])
+while(int(userInput) < 0 or int(userInput) > (listLength - 1)):
+  userInput = int(input("Wybierz numer osoby: "))
+
+print(f"Imie: {personList[0][userInput]}\nNazwisko: {personList[1][userInput]}\nWiek: {personList[2][userInput]}\nKierunek studiów: {personList[3][userInput]}")
+
+
+###Przykład: dodawanie nowego klucza do słownika##########################
+# cars_countries['Korea'] = 'Kia' # 1 sposób dodawanie nowego klucza do słownika
+# print(cars_countries)
+# print(cars_countries['Korea'])
+
+# 2 sposób dodawanie nowego/ch klucza/kluczy do słownika
+# cars_countries.update({
+#     'France': 'Citroen',
+#     'Spain': 'Seat'
+#    })
+# print(cars_countries)
+
+######################Zadanie 3
+# Do poprzednio utworzonego słownika dodaj nowy klucz o nazwie "kierunek_studiów", wartość w/w klucza dowolna
+# wskazana przez użytkownika
+
+
+################# Metody dla klasy słownik:
+### var_dict.clear() - usuwa wszystkie klucze ze słownika var_dict
+### var_dict.get(klucz) - zwraca wartość dla podanego klucza, gdy klucza nie ma w słowniku zwraca None
+### var_dict.has_key(klucz) - zwraca True gdy klucz jest w słowniku, False w przeciwnym razie,
+### var_dict.items() - zwraca listę dwuelementowych krotek (klucz, wartość)
+### var_dict.keys() - zwraca listę wszystkich kluczy
+### var_dict.values() - zwraca listę wszystkich wartości
+
+################# Przykłady
+# cars_countries = {
+#       'Germany': ['Audi', 'BMW'],
+#       'Asia': {'Japan': ['Toyota', 'Honda']}
+# }
+
+# del cars_countries['Germany'] # funkcja del usuwa klucz 'Germany' z słownika cars_countries
+# print(cars_countries)
+# keys = cars_countries.keys() 
+# print(keys)
+# l = len(keys) # funkcja len liczy długość, ilość elementów
+# print(l)
+# print(len(cars_countries))
+######################Zadanie 4
+# Wyświetl nazwy kluczy poprzednio utworzonego słownika, oraz ilość jego elementów
+# ###########################################
+
+##############  Zadania do wykonania
 # 1. Wykonaj odejmowanie, mnożenie i dzielenie 2 dowolnych liczb
 def add(x, y):
   return x + y
@@ -96,6 +264,7 @@ def wyrazenie(a, b):
 print(wyrazenie(a, b))
 
 # 3. Popraw zmienną zdanie tak aby wyświetlany był napis: "Ala ma kota"
+zdanie = "Ala ma kota"
 print(zdanie)
 # 4. Wyświetl zdanie "Jestem a b mam c lat studiuję d",
 #  gdzie : a-imie, a-nazwisko, c-liczba, d-kierunek studiów są dowolne zmienne które podaje użytkownik (wczytywane z klawiatury)
@@ -149,13 +318,17 @@ if 1 == 0:
 # 1 == 1
 if 1 == 1:
   print("1 == 1") 
-# 1 != 0
-# 1 != 1
-#(x > 1 and x < 13) and x != 5  , dla x = 2
+
+## 8. Dla dowolnego x sprawdź wynik działań (x > 1 and x < 13) oraz (x != 5 or x < 0)
+zmiennaX = float(input("Podaj x: "))
+if ((x > 1 and x < 13) or (x != 5 or x < 0)) {
+  print(f"podana zmienna x jest w zakresie 1 < x < 13, x != 5, x < 0")
+}
+
 # Zadania dodatkowe:
 # 1. Wykonaj mini ankietę tj. poproś użytkownika o następujące informacje: imie, nazwisko, wiek, zadaj mu pytania: "Czy zdrowo się odżywiasz?",
 # , "Czy lubisz sport?" i dodatkowo 3 inne własne. Po uzyskaniu wszystkich odpowiedzi wyświetl ich podsumowanie.
-# 2. Twoim zadaniem jest przygotowanie uniwersalnego i profesjonalnego życiorysu, złożonego z 10-ciu zdań, które wyświetlisz na ekranie
+# 2. Twoim zadaniem jest przygotowanie uniwersalnego i profesjonalnego życiorysu, złożonego z 5-ciu zdań, które wyświetlisz na ekranie
 # Użytkownik wpisuje tylko swoje imie, nazwisko, wiek, zawód, miejsce urodzenia, zainteresowania i ... życiorys jest gotowy.
 # 3. Przygotuj dla dziecka, które uczy się czytać zestaw sylab do nauki, ale zrób to inteligentnie tj.
 # dziecko wpisuje na klawiaturze 1 spółgłoskę a Ty dodajesz do niej odpowiednie samogłoski i wyświetlasz całość na ekranie
